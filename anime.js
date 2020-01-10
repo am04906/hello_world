b232cac764dab8168f6a6ad601a26a43950dcaca
am04906 <59737003+am04906@users.noreply.github.com>
2020-01-10 11:59:46 -0500

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,5 +1,8 @@
+let containerNode = document.getElementById("container_for_animation");
+let animeID = "animate_id";
+
 function myMove() {
-    let elem = document.getElementById("animate");
+    let elem = document.getElementById(animeID);
     let pos = 0;
     let id = setInterval(frame, 5);
 
@@ -13,3 +16,21 @@ function myMove() {
         }
     }
 }
+
+
+// add a button to start the animation
+let buttonNode = document.createElement('input');
+buttonNode.setAttribute('type', 'button');
+buttonNode.setAttribute('value', 'Start');
+buttonNode.onclick = myMove;
+//buttonNode.setAttribute('onclick', myMove);
+containerNode.appendChild(buttonNode);
+
+
+// add container for the animation box
+let boxNode = document.createElement("div");
+boxNode.setAttribute('id', animeID);
+containerNode.appendChild(boxNode); // Append <li> to <ul> with id="myList"
+
+
+alert(document.body.innerHTML);
